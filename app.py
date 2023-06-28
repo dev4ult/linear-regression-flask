ea600d5a71abd901f66c3d7f8c353686f0c38c1b
dev4ult <nibrasbiasaaja@gmail.com>
2023-06-28 22:04:20 +0700

    minor changes

--- app.py
+++ app.py
@@ -5,22 +5,32 @@ from source.regression import predict_discount
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/example", methods=["GET", "POST"])
+def example():
     if request.method == "POST":
         price = request.form["price"]
         if price != "":
             get_discount = predict_discount(price)
             return render_template("index.html", discount=get_discount, price=price)
         else:
-            return redirect(url_for("index"))
+            return redirect(url_for("example"))
     else:
-        return render_template("index.html")
+        return render_template("example.html")
 
 
 @app.route("/documentation")
 def docs():
     return render_template("documentation.html")
+
+
+@app.route("/<not_found>")
+def notfound(not_found):
+    return render_template("notfound.html", page=not_found)
 
 
 if __name__ == "__main__":
---
+++ static/script/uploadFile.js
@@ -0,0 +1,4 @@
+const uploadContainer = document.getElementById('upload-container');
+const uploadFile = document.getElementById('upload-file');
+
+function fileHandler(file, name, type) {}
